f68e75cdbd74539885c01f8d6bb8f0ef7308e4c4
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-11-09 10:42:57 -0800

    d

--- assignment.py
+++ assignment.py
@@ -1,5 +1,39 @@
 #!python
+import sqlite3 
 
+file = 'dbase.db'
+conn  =  sqlite3.connect('dbase.db')
+cursor = conn.cursor()
+x = input('pet name')
+c = input('pet species')
+v = input('pet breed')
+b = input('owner name')
+n = input('owner phone number')
+m = input('owner email')
+a = input('owner balance')
+s = input('date of first visit(mm/dd/yy)')
+data = str([x,c,v,b,n,m,a,s])
+cursor.execute(data)
+conn.commit ()
+
+"""
+file = 'dbase.db'
+with open(file, 'w') as my_file:
+
+
+
+ data = [x,c,v,b,n,m,a,s]
+ my_file.write(str(data))
+connection = sqlite3.connect(file)
+print(connection)
+cursor = connection.cursor()
+f = ' '.join(data)
+print(data)
+cursor.execute(f)
+connection.commit()
+result = cursor.fetchall()
+print(result)
+"""
 """
 Create a program that will store the database for a veterinary
 Each record needs to have the following information:
