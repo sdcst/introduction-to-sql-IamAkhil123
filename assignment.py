835805e6abf50c86e1a7b36044a2e400e117251f
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-11-29 10:24:25 -0800

    hjvj
    kkjbjkh

--- assignment.py
+++ assignment.py
@@ -1,28 +1,43 @@
 #!python
-
+"""
 import sqlite3
-file = 'dbase.db'
+
+file = 'assignment.db'
 connection = sqlite3.connect(file)
 print(connection)
-
-mydb = sqlite3.connector.connect(
+mydb = sqlite3.connect(
   host="localhost",
   user="myusername",
-  password="mypassword"
+  password="mypassword",
+  database="mydatabase"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
+sql = "INSERT INTO customers (pet name, pet species, pet breed, owner name, owner phone number, owner email, owner balance,    ) VALUES (%s, %s)"
+val = ("John", "Highway 21")
 
-#If this page is executed with no error, you have successfully created a database.
+mycursor.execute(sql, val)
 
+mydb.commit()
 
+print(mycursor.rowcount, "record inserted.")
 
+cursor = connection.cursor()
+query = 
+create table if not exists customers (
+    id integer primary key autoincrement,
+    pet name tinytext,
+    pet species tinytext,
+    pet breed tinytext,
+    owner name tinytext,
+    owner phone number
+    owner email
+    owner balance
+    date of first visit(mm/dd/yy));
 
+cursor.execute('PRAGMA table_info(customers);')
 
-
-"""
 x = str(input('pet name'))
 c = str(input('pet species'))
 v = str(input('pet breed'))
@@ -31,23 +46,45 @@ n = str(input('owner phone number'))
 m = str(input('owner email'))
 a = str(input('owner balance'))
 s = str(input('date of first visit(mm/dd/yy)'))
-data = [x,c,v,b,n,m,a,s]
-
-cursor = connection.cursor()
-query = 
-create table if not exists customers (
-    id integer primary key autoincrement,
-    pet name tinytext,
-    pet specices tinytext,
-    owner name tinytext,
-    owner phonenumber tinytext, 
-    owner email tinytext,
-    owner balance tinytext,
-    data of first visit tinytext,
-    cnum int);
+data1 = [x,c,v,b,n,m,a,s]
+data = data1
+for i in data:
+    query = f"insert into customers (pet name,pet species,pet breed,owner name,owner phone number,owner email,owner balance,date of first visit(mm/dd/yy)) values ('{i[0]}','{i[1]}','{i[2]}','{i[3]}','{i[4]}','{i[5]}','{i[6]}','{i[7]}');"
+    print(query)
+    cursor.execute(query)
+connection.commit()
+query = "select * from customers"
+cursor.execute(query)
+result = cursor.fetchall()
+print(result)
+for i in result:
+    print(i)
 """
 
 
+import  sqlite3
+conn  =  sqlite3 . connect ( 'assignment.db' )
+cursor  =  conn.cursor ()
+#create the salesman table 
+cursor.execute("CREATE TABLE customers(petname,petspecies,petbreed,ownername,ownerphonenumber,owneremail,ownerbalance;")
+
+petname = input('Salesman ID:')
+petspecies = input('Name:')
+petbreed = input('City:')
+ownername = input('Commission:')
+ownerphonenumber = input('ownerphonenumber:')
+owneremail = input('owneremail:')
+ownerbalance = input('ownerbalance:')
+cursor.execute("""
+INSERT INTO salesman(salesman_id, name, city, commission)
+VALUES (?,?,?,?)
+""", (petname, petspecies, petbreed, ownername, ownerphonenumber, owneremail, ownerbalance))
+conn.commit ()
+print ( 'Data entered successfully.' )
+conn . close ()
+if (conn):
+  conn.close()
+  print("\nThe SQLite connection is closed.")
 
 """
 Each record needs to have the following information:
